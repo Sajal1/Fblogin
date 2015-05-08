c38150e67e50d1a34e7032d670edb5de6797d874
sajalchowdhury <sajalchowdhry@gmail.com>
2015-05-08 23:02:57 +0600

    second commit

--- app/src/main/java/fblogin/example/com/fblogin/MainActivity.java
+++ app/src/main/java/fblogin/example/com/fblogin/MainActivity.java
@@ -12,8 +12,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //made some changes
+        //made changes
 
     }
 
