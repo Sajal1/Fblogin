47209f43eea38cd0edc4893c1260443f3d7365fe
sajalchowdhury <sajalchowdhry@gmail.com>
2015-05-09 09:53:44 +0600

    facebook sdk in oncreate and import

--- app/src/main/java/fblogin/example/com/fblogin/MainActivity.java
+++ app/src/main/java/fblogin/example/com/fblogin/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.facebook.FacebookSdk;
 
 
 public class MainActivity extends Activity {
@@ -12,7 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        FacebookSdk.sdkInitialize(getApplicationContext());
 
         //made changes last
 
